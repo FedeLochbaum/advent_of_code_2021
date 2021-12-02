68ade6515ab48456fadc39e53373f12d04a96008
Federico Lochbaum <federico.lochbaum@gmail.com>
2021-12-02 00:18:43 -0300

    day 1

---
+++ challenges/Day 1: Sonar Sweep/sonar_sweep.py
@@ -0,0 +1,36 @@
+
+input_path = 'advent_of_code_2021/challenges/Day 1: Sonar Sweep/input.txt'
+# Part 1
+times = -1
+last = float('-inf')
+with open(input_path) as f:
+  for line in f:
+    current = float(line)
+    if (current > last):
+      times+=1
+    last = current
+
+print(times)
+
+# Part 2
+times = 0
+i = 0
+window = { 0: 0, 1: 0, 2: 0, 'sum': 0 }
+
+# TODO: only use a string and shift it
+def add(num):
+  window['sum'] = (window['sum'] - window[2]) + num
+  window[2] = window[1]
+  window[1] = window[0]
+  window[0] = num
+
+with open(input_path) as f:
+  for line in f:
+    i+=1
+    current = float(line)
+    sum = window['sum']
+    add(current)
+    if ( i > 3 and window['sum'] > sum ):
+      times+=1
+
+print(times)
