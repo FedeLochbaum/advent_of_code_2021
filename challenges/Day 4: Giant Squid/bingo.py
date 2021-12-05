1f02ed95a9d84dea096472fee4c620f85922264f
Federico Lochbaum <federico.lochbaum@gmail.com>
2021-12-05 16:31:20 -0300

    Day 4 part 2

--- challenges/Day 4: Giant Squid/bingo.py
+++ challenges/Day 4: Giant Squid/bingo.py
@@ -1,20 +1,20 @@
 from functools import reduce  
-input_path = 'advent_of_code_2021/challenges/Day 4: Giant Squid/test'
+input_path = 'advent_of_code_2021/challenges/Day 4: Giant Squid/input'
 
 numbers = []
 boards = []
+wins = []
 class Board:
   def __init__(self):
     self.rows = []
     self.index = {} # numbers index -> (row, col, marked)
+    self.won = False
 
   def add_row(self, row):
-    for y, val in enumerate(row):
-      self.index[val] = [len(self.rows), y, False]
+    for y, val in enumerate(row): self.index[val] = [len(self.rows), y, False]
     self.rows.append(row)
 
-  def won_row(self, x):
-    return all(self.index[cell][2] for cell in self.rows[x])
+  def won_row(self, x): return all(self.index[cell][2] for cell in self.rows[x])
 
   def won_column(self, y):
     for x in range(5):
@@ -22,19 +22,14 @@ class Board:
     return True
 
   def annotate(self, val):
-    if (self.index[val]):
+    if val in self.index:
       x, y, _ = self.index[val]
       self.index[val][2] = True
-      return self.won_row(x) or self.won_column(y)
+      self.won = self.won_row(x) or self.won_column(y)
+      return self.won
     return False
 
-  def unmarked_sum(self):
-    res = 0
-    for key in self.index.keys():
-      if(not self.index[key][2]):
-        res += int(key)
-    return res
-    # return reduce(lambda n, key: n + int(key) if not self.index[key][2] else 0, self.index.keys(), 0)
+  def unmarked_sum(self): return reduce(lambda n, key: n + (int(key) if not self.index[key][2] else 0), self.index.keys(), 0)
 
 # Part 1
 with open(input_path) as f:
@@ -54,12 +49,10 @@ with open(input_path) as f:
       current_board.add_row(line.split())
 
   # Solving
-  cut = False
   for num in numbers:
-    print('num: ', num)
     for board in boards:
-      if (board.annotate(num)):
-        print(board.unmarked_sum() * int(num))
-        cut = True
-        break
-    if (cut): break
+      if (board.won): continue
+      if (board.annotate(num)): wins.append(board.unmarked_sum() * int(num))
+
+  print(wins[0])
+  print(wins[-1])
