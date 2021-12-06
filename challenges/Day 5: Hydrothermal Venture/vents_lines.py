c98f834851c64fdf3224ae226369a78eb4c39d76
Federico Lochbaum <federico.lochbaum@gmail.com>
2021-12-06 10:56:09 -0300

    removing unnecesary comments

--- challenges/Day 5: Hydrothermal Venture/vents_lines.py
+++ challenges/Day 5: Hydrothermal Venture/vents_lines.py
@@ -13,18 +13,16 @@ def split_line(line):
 
 def check(points):
   for x, y in points:
-    if (matrix[x][y] > 0): collisions.add(str(x) + ',' + str(y))
+    if matrix[x][y] > 0: collisions.add(str(x) + ',' + str(y))
     matrix[x][y] += 1
 
 with open(input_path) as f:
   for line in f:
     x1, y1, x2, y2, minX, minY, maxX, maxY = split_line(line)
-    if (x1 == x2): check([[x1, y] for y in range(minY, maxY + 1)])
-    elif (y1 == y2): check([[x, y1] for x in range(minX, maxX + 1)])
+    if x1 == x2: check([[x1, y] for y in range(minY, maxY + 1)])
+    elif y1 == y2: check([[x, y1] for x in range(minX, maxX + 1)])
     else: 
       _x1, _y1, _x2, _ = [x1, y1, x2, y2] if minY == y1 else [x2, y2, x1, y2]
       check([[_x1 - i, _y1 + i] if _x1 > _x2 else [_x1 + i, _y1 + i] for i in range(abs(_x2 - _x1) + 1)])
 
 print(len(collisions))
-# 5294
-# 21698
