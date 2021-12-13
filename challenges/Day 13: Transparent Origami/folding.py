93864efaa9eb3d4ebd5a414da97fd32dd482e08b
Federico Lochbaum <federico.lochbaum@gmail.com>
2021-12-13 13:21:25 -0300

    part 2 refactor

--- challenges/Day 13: Transparent Origami/folding.py
+++ challenges/Day 13: Transparent Origami/folding.py
@@ -3,24 +3,23 @@ import re
 ROW = 892; COL = 1310
 sum = 0
 matrix = [['.' for _ in range(COL)] for _ in range(ROW)]
+
 def fold_horizontal(n):
-  for i in range(n + 1, ROW):
+  for i in range(n + 1, min(ROW, ((n+1) * 2) - 1)):
     _row = n - (i - n)
-    if (_row < 0): continue
     for col, v in enumerate(matrix[i]): matrix[_row][col] = v if (v == '#') else matrix[_row][col]
   del matrix[n:ROW]
 
 def fold_vertical(n):
   for r, _ in enumerate(matrix):
-    for col in range(n + 1, COL):
+    for col in range(n + 1, min(COL, ((n+1) * 2) - 1)):
       _col = n - (col - n)
-      if (_col < 0): continue
       matrix[r][_col] = '#' if (matrix[r][col] == '#') else matrix[r][_col]
     del matrix[r][n:COL]
 
 with open(input_path) as f:
   for line in f:
-    if line == '\n': complete = True; continue
+    if line == '\n': continue
     if (re.match('fold along ', line)):
       dir, n = line[len('fold along '):].split('=')
       (fold_horizontal if dir == 'y' else fold_vertical)(int(n))
