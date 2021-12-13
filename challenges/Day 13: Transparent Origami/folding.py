3864f5093d7d06261a3d293679ccaf1e3d6c6747
Federico Lochbaum <federico.lochbaum@gmail.com>
2021-12-13 13:12:31 -0300

    part 2

--- challenges/Day 13: Transparent Origami/folding.py
+++ challenges/Day 13: Transparent Origami/folding.py
@@ -5,14 +5,16 @@ sum = 0
 matrix = [['.' for _ in range(COL)] for _ in range(ROW)]
 def fold_horizontal(n):
   for i in range(n + 1, ROW):
-    for col, v in enumerate(matrix[i]):
-      matrix[n - (i - n)][col] = v if (v == '#') else matrix[n - (i - n)][col]
+    _row = n - (i - n)
+    if (_row < 0): continue
+    for col, v in enumerate(matrix[i]): matrix[_row][col] = v if (v == '#') else matrix[_row][col]
   del matrix[n:ROW]
 
 def fold_vertical(n):
   for r, _ in enumerate(matrix):
     for col in range(n + 1, COL):
       _col = n - (col - n)
+      if (_col < 0): continue
       matrix[r][_col] = '#' if (matrix[r][col] == '#') else matrix[r][_col]
     del matrix[r][n:COL]
 
@@ -21,9 +23,7 @@ with open(input_path) as f:
     if line == '\n': complete = True; continue
     if (re.match('fold along ', line)):
       dir, n = line[len('fold along '):].split('=')
-      (fold_horizontal if dir == 'y' else fold_vertical)(int(n)); break
+      (fold_horizontal if dir == 'y' else fold_vertical)(int(n))
     else: col, row = line[:-1].split(','); matrix[int(row)][int(col)] = '#'
 
-for row in matrix: sum +=''.join(row).count('#')
-print(sum)
-# for row in matrix: print(row)
+for row in matrix: print(''.join(row))
