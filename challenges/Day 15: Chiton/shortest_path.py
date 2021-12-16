ee3ff23a63fd3cc71f606a6ad270bedc95ce10a1
Federico Lochbaum <federico.lochbaum@gmail.com>
2021-12-16 06:50:41 -0300

    part 2

--- challenges/Day 15: Chiton/shortest_path.py
+++ challenges/Day 15: Chiton/shortest_path.py
@@ -4,4 +4,14 @@ from utils import shortest_path, dict_from_file, point_by
 ROW = 100
 COL = 100
 
-print(shortest_path(dict_from_file(input_path), ROW, COL, point_by(0, 0), point_by(99, 99)))
+# print(shortest_path(dict_from_file(input_path), ROW, COL, point_by(0, 0), point_by(99, 99)))
+graph0 = dict_from_file(input_path)
+graph = [[ 0 for _ in range(COL * 5)] for _ in range(ROW * 5)]
+for row in range(ROW * 5):
+  for col in range(COL * 5):
+    original_i = row % ROW
+    original_c = col % COL
+    n = (row // ROW) + (col // COL) - 1
+    graph[row][col] = (graph0[original_i][original_c] + n) % 9 + 1
+
+print(shortest_path(graph, ROW * 5, COL * 5, point_by(0, 0), point_by(499, 499)))
