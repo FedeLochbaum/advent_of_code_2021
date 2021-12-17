daab5644006f8c272398af18a077f3e79f636ccc
Federico Lochbaum <federico.lochbaum@gmail.com>
2021-12-16 22:28:05 -0300

    day 16

--- challenges/Day 16: Packet Decoder/parsing.py
+++ challenges/Day 16: Packet Decoder/parsing.py
@@ -1,35 +1,21 @@
-input_path = 'advent_of_code_2021/challenges/Day 16: Packet Decoder/test'
-from utils import hex_to_binary
+input_path = 'advent_of_code_2021/challenges/Day 16: Packet Decoder/input'
+from utils import hex_to_binary, operator_node, num_literal_node, op_by_id, eval
 
-def operator_node(op, children, version): return { 'type': 'OP', 'op': op, 'chilcren': children, 'version': version }
-def num_literal_node(num, version): return { 'type': 'NUM', 'value': num, 'version': version }
-
-def op_by_id(id): return id #TODO
-
-def consume_packet_by(id):
-  if (id == '100'): return consume_literal_value
-  return consume_operator
-
-def consume_number_in_range(buffer, i, j):
-  return int(buffer[i:j], 2)
+def consume_packet_by(id): return (consume_literal_value if id == '100' else consume_operator)
+def consume_number_in_range(buffer, i, j): return int(buffer[i:j], 2)
 
 def consume_operator(buffer, i, version, id):
-  print('version, id: ', version, id)
   sub_packets = []
   if buffer[i] == '0':
     total_length_in_bits = consume_number_in_range(buffer, i+1, i+16)
-    print('voy por total_length: ', total_length_in_bits)
-    i+=16
-    j=0
+    i+=16; j=0
     while (j != total_length_in_bits):
       ast, _i = consume_packet(buffer, i)
-      print('consumi: ', _i)
       sub_packets.append(ast)
       j+=(_i-i)
       i=_i
   else:
     number_of_sub_packets = consume_number_in_range(buffer, i+1, i+12)
-    print('voy por number_of_sub_packets: ', number_of_sub_packets)
     i+=12
     for _ in range(number_of_sub_packets):
       ast, _i = consume_packet(buffer, i)
@@ -38,29 +24,20 @@ def consume_operator(buffer, i, version, id):
 
   return operator_node(op_by_id(id), sub_packets, version), i
 
-# todo gud
 def consume_literal_value(buffer, i, version, _):
-  print('version, id: ', version)
-  while(buffer[i] == '0'): i+=1
   number = ''
   while(True):
     header = buffer[i]
     number = number + buffer[i+1:i+5]
     i+=5
     if (header == '0'): break
-  print('numerin: ',number, int(number, 2))
   return num_literal_node(int(number, 2), version), i
-
-# TODO: Goes consuming on demand the filee with a buffer
-input = hex_to_binary(open(input_path).readline())
 
 def consume_packet(buffer, i):
   version = buffer[i:i+3]
   id = buffer[i+3:i+6]
   return consume_packet_by(id)(buffer, i+6, int(version, 2), int(id, 2))
 
-ast = consume_packet(input, 0)
-
-# binary_string = binascii.unhexlify(string)
-
-# print(binary_string)
+input = hex_to_binary(open(input_path).readline())
+ast, i = consume_packet(input, 0)
+print(eval(ast))
--- challenges/Day 16: Packet Decoder/utils.py
+++ challenges/Day 16: Packet Decoder/utils.py
@@ -1,8 +1,32 @@
 
+import math
 simple_hex_to_bin = { '0': '0000', '1': '0001', '2': '0010', '3': '0011', '4': '0100',
   '5': '0101', '6': '0110', '7': '0111', '8': '1000', '9': '1001', 'A': '1010',
   'B': '1011', 'C': '1100', 'D': '1101', 'E': '1110', 'F': '1111'
 }
+
+op_by_id_obj = { 0: 'ADD', 1: 'TIMES', 2: 'MIN', 3: 'MAX', 5: 'GT', 6: 'LT', 7: 'EQ' }
+
+apply_to_children = lambda f: lambda ast: f(map(eval, ast['children']))
+apply_comp = lambda f: lambda ast: 1 if f(eval(ast['children'][0]), eval(ast['children'][1])) else 0
+get_value = lambda ast: ast['value']
+
+eval_function_by_type = {
+  'ADD': apply_to_children(sum),
+  'TIMES': apply_to_children(math.prod),
+  'MIN': apply_to_children(min),
+  'MAX': apply_to_children(max),
+  'NUM': get_value,
+  'GT': apply_comp(lambda x, y: x > y),
+  'LT': apply_comp(lambda x, y: x < y),
+  'EQ': apply_comp(lambda x, y: x == y),
+}
+
+def eval(ast): return eval_function_by_type[ast['type']](ast)
+def operator_node(op, children, version): return { 'type': op, 'children': children, 'version': version }
+def num_literal_node(num, version): return { 'type': 'NUM', 'value': num, 'version': version }
+
+def op_by_id(id): return op_by_id_obj[id]
 
 def hex_to_binary(hex_srt):
   res = ''
