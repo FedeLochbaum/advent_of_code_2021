ffe1ef0f004317fa185f0a36c0d80792d6aaec36
Federico Lochbaum <federico.lochbaum@gmail.com>
2021-12-19 19:27:40 -0300

    advances

--- challenges/Day 18: Snailfish/math_homework.py
+++ challenges/Day 18: Snailfish/math_homework.py
@@ -1,92 +1,43 @@
-input_path = 'advent_of_code_2021/challenges/Day 18: Snailfish/test0'
-from ast import literal_eval
-from collections import deque
+input_path = 'advent_of_code_2021/challenges/Day 18: Snailfish/test1'
+from tree import tree, leaf, is_leaf, parse_tree, replace_node, find_to_explode, find_to_divide, any_literal_to_divide, find_right_leaf, find_left_leaf, as_list
 
-LEAF = 'LEAF'
-PAIR = 'PAIR'
+magnitude = lambda pair: pair['value'] if is_leaf(pair) else 3 * magnitude(pair['left']) +  2 * magnitude(pair['right'])
 
-literal = lambda value: { 'type': LEAF, 'value': value, 'height': 0 }
-pair = lambda left, right: { 'type': PAIR, 'left': left, 'right': right, 'height': max(left['height'], right['height']) + 1 }
-is_leaf = lambda pair: pair['type'] == LEAF
-is_pair = lambda pair: pair['type'] == PAIR
-
-def any_literal_to_divide(parent):
-  if (is_leaf(parent)): return parent['value'] > 9
-  return any_literal_to_divide(parent['left']) or any_literal_to_divide(parent['right'])
-
-def update_by_path(parent, path, new_value):
-  queue = deque(path)
-  current = parent
-  if len(path) == 0: return
-  while(len(path) > 1):
-    index = queue.popleft()
-    current = current['left' if index == 0 else 'right']
-  last = queue.popleft()
-  current['left' if last == 0 else 'right'] = new_value
-
-def find_by_root(parent, found, path = []):
-  if (found(parent, path)): return path, parent
-  left_path, l_child = find_by_root(parent['left'], found, path + [0])
-  if (left_path != None): return left_path, l_child
-  right_path, r_child = find_by_root(parent['right'], found, path + [1])
-  if (right_path != None): return [right_path, r_child]
-  return [None, None]
-
-# TODO: re think
-# def find_first_to_left_from(parent, path):
-  # if (path[-1] == 0):
-
-
-def find_by_leaf(child, found, path):
-  if (found(child, path)): return path, child
-  # if(path[-1] == 0) # 
-
-
-
-  # left_path, l_child = find_by_root(parent['left'], found, path + [0])
-  # if (left_path != None): return left_path, l_child
-  # right_path, r_child = find_by_root(parent['right'], found, path + [1])
-  # if (right_path != None): return [right_path, r_child]
-  # return [None, None]
-
-find_child_to_divide_path = lambda parent: find_by_root(parent, lambda node, _: is_leaf(node) and node['value'] > 9)
-find_explode_path = lambda parent: find_by_root(parent, lambda node, current_path: len(current_path) == 3 and is_pair(node))
-
-find_first_to_left_from = lambda parent: find_by_leaf(parent, lambda node, current_path: is_leaf(node) and current_path[-1] == 0)
-find_first_to_right_from = lambda parent: find_by_leaf(parent, lambda node, current_path: is_leaf(node) and current_path[-1] == 1)
+def add(pair1, pair2):
+  t = tree(pair1, pair2)
+  pair1['parent'] = t
+  pair2['parent'] = t
+  return reduce(t)
 
 def explode_child(parent):
-  path, p = find_explode_path(parent)
+  p = find_to_explode(parent)
   left = p['left']
   right = p['right']
-  update_by_path(parent, path, literal(0))
-  left_path, l_leaf = find_first_to_left_from(parent, path)
-  right_path, r_leaf = find_first_to_right_from(parent, path)
-  if(left_path != None): update_by_path(parent, left_path, literal(l_leaf['value'] + left['value']))
-  if (right_path != None): update_by_path(parent, right_path, literal(r_leaf['value'] + right['value']))
+  replace_node(p, leaf(0))
+  l_leaf = find_left_leaf(parent, 0) # len(path)
+  r_leaf = find_right_leaf(parent, 0)
+  if(l_leaf != None): l_leaf['value'] += left['value']
+  if (r_leaf != None): r_leaf['value'] += right['value']
+
   return parent
 
-def divide_child(parent):
-  path, leaf = find_child_to_divide_path(parent)
-  update_by_path(parent, path, pair(
-    literal(leaf//2),
-    literal(leaf//2 + (1 if leaf % 2 == 1 else 0))
+def divide_child(_tree):
+  l = find_to_divide(_tree)
+  value = l['value']
+  replace_node(l, tree(
+    leaf(value//2),
+    leaf(value//2 + (1 if value % 2 == 1 else 0))
   ))
-  return parent
+
+  return _tree
 
 def reduce(pair):
-  if (pair['height'] == 4): return reduce(explode_child(pair))
+  if (pair['height'] == 5):
+    return reduce(explode_child(pair))
   if (any_literal_to_divide(pair)): return reduce(divide_child(pair))
   return pair
 
-magnitude = lambda pair: pair['value'] if is_leaf(pair) else 3 * magnitude(pair['left']) +  2 * magnitude(pair['right'])
-add = lambda pair1, pair2: reduce(pair(pair1, pair2))
-
-parse_literal_pair = lambda val: pair(parse_literal_pair(val[0]), parse_literal_pair(val[1])) if type(val) == list else literal(val)
-parse_pair = lambda str: parse_literal_pair(literal_eval(str))
-
 with open(input_path) as f:
-  current = parse_pair(f.readline()[:-1])
-  for line in f:
-    current = add(current, parse_pair(line[:-1]))
+  current = parse_tree(f.readline()[:-1])
+  for line in f: current = add(current, parse_tree(line[:-1]))
   print(magnitude(current))
---
+++ challenges/Day 18: Snailfish/tree.py
@@ -0,0 +1,96 @@
+from collections import deque
+from ast import literal_eval
+
+LEAF = 'LEAF'
+PAIR = 'PAIR'
+
+leaf = lambda value, parent = None: { 'type': LEAF, 'value': value, 'height': 0, 'parent': parent }
+tree = lambda left, right, parent = None: { 'type': PAIR, 'left': left, 'right': right, 'height': max(left['height'], right['height']) + 1, 'parent': parent }
+is_leaf = lambda pair: pair['type'] == LEAF
+is_pair = lambda pair: pair['type'] == PAIR
+
+parse_literal_pair = lambda val: tree(parse_literal_pair(val[0]), parse_literal_pair(val[1])) if type(val) == list else leaf(val)
+parse_tree = lambda str: parse_literal_pair(literal_eval(str))
+
+def as_list(t):
+  if (is_leaf(t)): return t['value']
+  return [as_list(t['left']), as_list(t['right'])]
+
+def any_literal_to_divide(parent):
+  if (is_leaf(parent)): return parent['value'] > 9
+  return any_literal_to_divide(parent['left']) or any_literal_to_divide(parent['right'])
+
+def replace_node(to_replace, new_tree):
+  parent = to_replace['parent']
+  new_tree['parent'] = parent
+  if (parent['left'] == to_replace): parent['left'] = new_tree
+  else: parent['right'] = new_tree
+  parent['height'] = max(parent['left']['height'], parent['right']['height']) + 1
+
+def find_to_divide(current):
+  if(is_leaf(current) and current['value'] > 9): return current
+  
+  if(is_pair(current)):
+
+    l_child = find_to_divide(current['left'])
+    if (l_child != None): return l_child
+
+    r_child = find_to_divide(current['right'])
+    if (l_child != None): return r_child
+
+  return None
+
+def find_to_explode(parent, level = 0):
+
+  if(is_pair(parent)):
+    if (level == 5): return parent
+
+    l_child = find_to_explode(parent['left'], level + 1)
+    if (l_child != None): return l_child
+
+    r_child = find_to_explode(parent['right'], level + 1)
+    if (l_child != None): return r_child
+
+  return None
+
+def find_left_leaf(node, level = 0):
+  if (node == None or node['parent'] == None): return None
+
+  while (node['parent']['left'] == node or (node['parent']['left'] == None and node['parent']['right'] == node)):
+    if (node['parent'] == None): return None
+
+    node = node['parent']
+    level -= 1
+
+  node = node['parent']['left']
+
+  while (level < 0):
+    if (node['right'] != None): node = node['right']
+    elif (node['left'] != None): node = node['left']
+    else: break
+    level += 1
+
+  if (level == 0): return node
+
+  return find_right_leaf(node, level)
+
+def find_right_leaf(node, level = 0):
+  if (node == None or node['parent'] == None): return None
+
+  while (node['parent']['right'] == node or (node['parent']['right'] == None and node['parent']['left'] == node)):
+    if (node['parent'] == None): return None
+
+    node = node['parent']
+    level -= 1
+
+  node = node['parent']['right']
+
+  while (level < 0):
+    if (node['left'] != None): node = node['left']
+    elif (node['right'] != None): node = node['right']
+    else: break
+    level += 1
+
+  if (level == 0): return node    
+
+  return find_right_leaf(node, level)
