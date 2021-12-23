eb647cb93e3fdc2573b345d7f18447e4ef5ec023
Federico Lochbaum <federico.lochbaum@gmail.com>
2021-12-22 21:05:43 -0300

    some comments

--- challenges/Day 19: Beacon Scanner/beacons.py
+++ challenges/Day 19: Beacon Scanner/beacons.py
@@ -6,11 +6,13 @@ from functools import reduce
 from scanner import Scanner
 
 combinations = lambda array: reduce(lambda acc, elem: acc + [
-  elem, [-elem[0], -elem[1], -elem[2]],
-  [-elem[0], elem[1], elem[2]], [elem[0], -elem[1], elem[2]], [elem[0], elem[1], -elem[2]],
-  [-elem[0], -elem[1], elem[2]], [-elem[0], elem[1], -elem[2]],
-  [elem[0], -elem[1], -elem[2]]
+  (elem[0], elem[1], elem[2]), (-elem[0], -elem[1], -elem[2]), # all positive, all negative
+  (-elem[0], elem[1], elem[2]), (elem[0], -elem[1], elem[2]), (elem[0], elem[1], -elem[2]), # only one negative
+  (-elem[0], -elem[1], elem[2]), (-elem[0], elem[1], -elem[2]), (elem[0], -elem[1], -elem[2]) # two negatives
 ], list(itertools.permutations(array)), [])
+
+get_diff = lambda base_point, _point: (base_point[0] - _point[0], base_point[1] - _point[1], base_point[2] - _point[2])
+apply_diff = lambda point, diff: (diff[0] + point[0], diff[1] + point[1], diff[2] + point[2])
 
 scanners = deque()
 scanner_0 = None
@@ -25,32 +27,31 @@ with open(input_path) as f:
       else: scanners.append(current)
     else:
       point = line[:-1].split(',')
-      current.add_point([int(point[0]), int(point[1]), int(point[2])])
+      current.add_point((int(point[0]), int(point[1]), int(point[2])))
 
+# Warning! this is O(n^4)
 def try_connect(base_scanner, scanner_to_connect):
   for point in scanner_to_connect.points:
     all_commbinations = combinations(point)
-    for _point in all_commbinations: # Warning! this is n^3
+    for _point in all_commbinations:
       for base_point in base_scanner.points:
-        diff = [_point[0] - base_point[0], _point[1] - base_point[1], _point[2] - base_point[2]]
+        diff = get_diff(base_point, _point)
         with_offset = list(map(lambda p: apply_diff(p, diff), all_commbinations))
-        if len(list(set(scanner_to_connect.points) & set(with_offset)) >= 12):
-          return diff
+        intersection = set(scanner_to_connect.points).intersection(with_offset)
+        if len(intersection) >= 12: return diff
   return None
-
-def apply_diff(point, diff):
-  print('point: ', point)
-  return [point[0] - diff[0], point[1] - diff[1], point[2] - diff[2]]
 
 # Solution
 while(scanners):
   scanner = scanners.popleft()
   diff = try_connect(scanner_0, scanner)
   if (diff != None):
+    print('Descarto scanner: ', scanner.label)
     for point in scanner.points:
       real_point = apply_diff(point, diff)
       if (not real_point in scanner_0.points): scanner_0.add_point(real_point)
   else:
+    print('Fallé intentando integrar el scanner: ', scanner.label)
     scanners.append(scanner)
 
 print(len(scanner_0.points))
