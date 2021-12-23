4e6d12c623d534c3d786bf8a959f17e99c1acd93
Federico Lochbaum <federico.lochbaum@gmail.com>
2021-12-23 20:03:05 -0300

    some ideas

--- challenges/Day 19: Beacon Scanner/beacons.py
+++ challenges/Day 19: Beacon Scanner/beacons.py
@@ -5,16 +5,17 @@ from collections import deque
 from functools import reduce
 from scanner import Scanner
 
-combinations = lambda array: reduce(lambda acc, elem: acc + [
+negative_combinations = lambda elem: [
   (elem[0], elem[1], elem[2]), (-elem[0], -elem[1], -elem[2]), # all positive, all negative
   (-elem[0], elem[1], elem[2]), (elem[0], -elem[1], elem[2]), (elem[0], elem[1], -elem[2]), # only one negative
   (-elem[0], -elem[1], elem[2]), (-elem[0], elem[1], -elem[2]), (elem[0], -elem[1], -elem[2]) # two negatives
-], list(itertools.permutations(array)), [])
+]
 
+combinations = lambda array: reduce(lambda acc, elem: acc + negative_combinations(elem), list(itertools.permutations(array)), [])
 get_diff = lambda base_point, _point: (base_point[0] - _point[0], base_point[1] - _point[1], base_point[2] - _point[2])
-apply_diff = lambda point, diff: (diff[0] + point[0], diff[1] + point[1], diff[2] + point[2])
+apply_delta = lambda point, delta: (point[0] * delta[0], point[1] * delta[1], point[2] * delta[2])
 
-LOWER_BOUND = 12
+LOWER_BOUND = 11
 
 scanners = deque()
 scanner_0 = None
@@ -26,33 +27,41 @@ with open(input_path) as f:
       label = line[12]
       current = Scanner(label)
       if (label == '1'): scanner_0 = current
-      scanners.append(current)
+      else: scanners.append(current)
     else:
       point = line[:-1].split(',')
       current.add_point((int(point[0]), int(point[1]), int(point[2])))
 
+def check_diff(base_scanner, scanner, diff):
+  for delta in negative_combinations((1,1,1)):
+    count = 0
+    for p in scanner.points:
+      p_with_delta = apply_delta(p, delta)
+      point = (diff[0] - p_with_delta[0], diff[1] - p_with_delta[1], diff[2] + p_with_delta[2])
+      if point in base_scanner.points: count+=1
+
+    if(count > LOWER_BOUND): return diff, delta
+  return None, None
+  
 # Warning! this is O(n^4)
 def try_connect(base_scanner, scanner_to_connect):
   for point in scanner_to_connect.points:
     all_commbinations = combinations(point)
     for _point in all_commbinations:
       for base_point in base_scanner.points:
-        diff = get_diff(base_point, _point)
-        with_offset = list(map(lambda p: apply_diff(p, diff), scanner_to_connect.points)) # No, all combination es conn respecto a un solo punto, 
-        intersection = set(base_scanner.points).intersection(with_offset)
-        if ( len(intersection) > 1): print(len(intersection))
-        if len(intersection) >= LOWER_BOUND: return diff
-  return None
+        diff, delta = check_diff(base_scanner, scanner, get_diff(base_point, _point))
+        if diff != None: return diff, delta
+  return None, None
 
 # Solution
 while(scanners):
   scanner = scanners.popleft()
-  diff = try_connect(scanner_0, scanner)
+  diff, delta = try_connect(scanner_0, scanner)
   if (diff != None):
-    print('Descarto scanner: ', scanner.label)
-    print('diff :', diff)
+    print('diff que matchea:', diff, ' con scanner: ', scanner.label)
     for point in scanner.points:
-      real_point = apply_diff(point, diff)
+      p_with_delta = apply_delta(point, delta)
+      real_point = (diff[0] - p_with_delta[0], diff[1] - p_with_delta[1], diff[2] + p_with_delta[2])
       if (not real_point in scanner_0.points): scanner_0.add_point(real_point)
   else:
     print('Fallé intentando integrar el scanner: ', scanner.label)
