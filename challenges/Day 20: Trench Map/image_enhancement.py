76a7ee41276ccaf403dcbc41314cffc3169682f1
Federico Lochbaum <federico.lochbaum@gmail.com>
2021-12-26 23:08:01 -0300

    day 20

--- challenges/Day 20: Trench Map/image_enhancement.py
+++ challenges/Day 20: Trench Map/image_enhancement.py
@@ -1,18 +1,17 @@
-input_path = 'advent_of_code_2021/challenges/Day 20: Trench Map/test'
+input_path = 'advent_of_code_2021/challenges/Day 20: Trench Map/input'
 from functools import reduce
 
 enhancement = ''; image = []; to_bin = { '.': '0', '#': '1' }
 
-enhance = lambda input: [[ enhanced_value(r - 2, c - 2, input) for c in range(len(input[0]) + 4)] for r in range(len(input) + 4)]
-enhance_image = lambda input, times: reduce(lambda i, _: enhance(i), range(times), input)
+enhance = lambda input, it: [[ enhanced_value(r - 2, c - 2, input, it) for c in range(len(input[0]) + 4)] for r in range(len(input) + 4)]
+enhance_image = lambda input, times: reduce(lambda img, it: enhance(img, it), range(times), input)
 
-def enhanced_value(r, c, input):
+def enhanced_value(r, c, input, it):
+  default = '0' if it % 2 == 0 else '1'
   in_range = lambda p: p[0] in range(0, len(input)) and p[1] in range(0, len(input[0]))
   points = [ [r + r_delta, c + c_delta] for r_delta in range(-1, 2) for c_delta in range(-1, 2)]
-  binary = ''.join(map(lambda p: to_bin[input[p[0]][p[1]]] if in_range(p) else '0', points))
-  index = int(binary, 2)
-  if (not any(map(in_range, points))): return '.'
-  return enhancement[index]
+  binary = ''.join(map(lambda p: to_bin[input[p[0]][p[1]]] if in_range(p) else default, points))
+  return enhancement[int(binary, 2)]
 
 with open(input_path) as f:
   enhancement = f.readline()[:-1]
@@ -21,7 +20,5 @@ with open(input_path) as f:
     image.append(list(line[:-1]))
 
 count = 0
-img = enhance_image(image, 50)
-for line in img: count += sum(char == '#' for char in line)
-for line in img: print(''.join(line))
+for line in enhance_image(image, 50): count += sum(char == '#' for char in line)
 print(count)
