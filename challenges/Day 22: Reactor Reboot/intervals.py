e9ef9bdf7dcd7a2b82c777a15fb006051ec7c175
Federico Lochbaum <federico.lochbaum@gmail.com>
2022-01-01 15:15:17 -0300

    new idea

--- challenges/Day 22: Reactor Reboot/intervals.py
+++ challenges/Day 22: Reactor Reboot/intervals.py
@@ -1,48 +1,8 @@
-input_path = 'advent_of_code_2021/challenges/Day 22: Reactor Reboot/test'
-from intervaltree import IntervalTree
-WIDTH = 50; HEIGHT = 50; DEPTH = 50
-interval_limit = [[-WIDTH, -HEIGHT, -DEPTH], [WIDTH, HEIGHT, DEPTH]]
+input_path = 'advent_of_code_2021/challenges/Day 22: Reactor Reboot/test0'
+from utils import min_max_sub_str, Interval, Point
+from functools import reduce
 
-def threeDtoOneD(_int): return _int[0] + (_int[1] * HEIGHT) + (_int[2] * DEPTH * HEIGHT)
-
-def oneDtoThreeD(_x):
-  z = _x // (DEPTH * HEIGHT)
-  _x -= z * DEPTH * HEIGHT
-  y = _x // HEIGHT
-  x = _x % WIDTH
-  return [x, y, z]
-
-class Interval:
-  def __init__(self, _min, _max):
-    self.min = _min
-    self.max = _max
-
-class Point:
-  def __init__(self, _int):
-    self.graph = {}
-    self.int = _int
-    self.oneD = None
-
-  def toOneD(self):
-    self.oneD = self.oneD or threeDtoOneD(self.shifted())
-    return self.oneD
-
-  def toThreeD(self): return self.unshifted(oneDtoThreeD(self.toOneD()))
-
-  def shifted(self): return [self.int[0] + WIDTH, self.int[1] + HEIGHT, self.int[2] + DEPTH]
-  def unshifted(self, _int): return [_int[0] - WIDTH, _int[1] - HEIGHT, _int[2] - DEPTH]
-
-min_max_sub_str = lambda x: x[2:].split('..')
-
-node_by_interval = lambda _int, left, right: { 'int': _int, 'key': _int.low, 'max': max(_int.high, left.max, right.max) } 
-
-in_range_limit = lambda inverval: (
-  inverval[0][0] >= interval_limit[0][0] and inverval[1][0] <= interval_limit[1][0] and
-  inverval[0][1] >= interval_limit[0][1] and inverval[1][1] <= interval_limit[1][1] and
-  inverval[0][2] >= interval_limit[0][2] and inverval[1][2] <= interval_limit[1][2]
-)
-
-tree = IntervalTree()
+commands = []
 with open(input_path) as f:
   for line in f:
     on = line[0:2] == 'on'
@@ -50,13 +10,19 @@ with open(input_path) as f:
     x_min, x_max = min_max_sub_str(x)
     y_min, y_max = min_max_sub_str(y)
     z_min, z_max = min_max_sub_str(z)
-    interval = [[int(x_min), int(y_min), int(z_min)], [int(x_max), int(y_max), int(z_max)]]
-    if (in_range_limit(interval)):
-      min = [int(x_min), int(y_min), int(z_min)]
-      max = [int(x_max), int(y_max), int(z_max)]
-      interval = Interval(Point(min), Point(max))
-      if on: tree.addi(interval.min.toOneD(), interval.max.toOneD(), interval)
-      else: tree.remove_overlap(interval.min.toOneD(), interval.max.toOneD())
+    min = [int(x_min), int(y_min), int(z_min)]
+    max = [int(x_max), int(y_max), int(z_max)]
+    commands.append((on, Interval(Point(min), Point(max))))
 
-for i in tree.items():
-  print(i)
+merge = []
+cuboids = []
+for cmd in commands:
+  on, interval = cmd
+  if on: merge.append((cmd, interval))
+  for c in cuboids:
+    intersect = c[1].intersect(interval)
+    if intersect: merge.append((False, intersect))
+  cuboids = cuboids + merge.copy()
+  # std::ranges::copy(merge, std::back_insert_iterator(cuboids))
+
+print(reduce(lambda acc, cmd: acc + ((1 if cmd[0] else -1) * cmd[1].count()), cuboids, 0))
--- challenges/Day 22: Reactor Reboot/utils.py
+++ challenges/Day 22: Reactor Reboot/utils.py
@@ -1,17 +1,28 @@
 min_max_sub_str = lambda x: x[2:].split('..')
 
-WIDTH = 50; HEIGHT = 50; DEPTH = 50
-interval_limit = [[-WIDTH, -HEIGHT, -DEPTH], [WIDTH, HEIGHT, DEPTH]]
+class Point:
+  def __init__(self, _int):
+    self.graph = {}
+    self.int = _int
+class Interval:
+  def __init__(self, _min, _max):
+    self.min = _min
+    self.max = _max
 
-node_by_interval = lambda _int, left, right: { 'int': _int, 'key': _int.low, 'max': max(_int.high, left.max, right.max) } 
+  def count(self): return (
+    abs(self.max.int[0] - self.min.int[0] + 1) *
+    abs(self.max.int[1] - self.min.int[1] + 1) *
+    abs(self.max.int[2] - self.min.int[2] + 1)
+  )
+
+  def intersect(self, interval):
+    min_point = Point([max(self.min.int[0], interval.min.int[0]), max(self.min.int[1], interval.min.int[1]), max(self.min.int[2], interval.min.int[2])])
+    max_point = Point([min(self.max.int[0], interval.max.int[0]), min(self.max.int[1], interval.max.int[1]), min(self.max.int[2], interval.max.int[2])])
+    if (min_point.int[0] > max_point.int[0] or min_point.int[1] > max_point.int[1] or min_point.int[2] > max_point.int[2]): return None
+    return Interval(min_point, max_point)
 
 point_in_range = lambda point, _range: (
   point[0] in range(_range[0][0], _range[1][0] + 1) and
   point[1] in range(_range[0][1], _range[1][1] + 1) and
   point[2] in range(_range[0][2], _range[1][2] + 1)
-)
-
-illegal_interval = lambda interval: (
-  interval[0][0] < interval_limit[0][0] or interval[0][1] < interval_limit[0][1] or interval[0][2] < interval_limit[0][2] or 
-  interval[1][0] > interval_limit[1][0] or interval[1][1] > interval_limit[1][1] or interval[1][2] > interval_limit[1][2]
 )
