f67a751497aae9b72841cee6e065a9b44f35b637
Federico Lochbaum <federico.lochbaum@gmail.com>
2022-01-02 15:29:02 -0300

    almost there

--- challenges/Day 23: Amphipod/game.py
+++ challenges/Day 23: Amphipod/game.py
@@ -3,29 +3,30 @@ empty_hallway = ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.']
 
 initial_state = lambda a, b, c, d: (a, b, c, d, empty_hallway.copy())
 step_cost_by = { 'A': 1, 'B': 10, 'C': 100, 'D': 1000 }
-room_pos = { 'A': 3, 'B': 5, 'C': 7, 'D': 9}
+room_pos = { 'A': 2, 'B': 4, 'C': 6, 'D': 8 }
 goal = { 'A': 0, 'B': 1, 'C': 2, 'D': 3 }
 
-is_goal = lambda state: state == (['A', 'A'], ['B', 'B'], ['C', 'C'], ['D', 'D'], empty_hallway)
+is_goal = lambda state: state == [['A', 'A'], ['B', 'B'], ['C', 'C'], ['D', 'D'], empty_hallway]
 
 def hallway_actions_by_index(state, index, elem, steps_until_hallway, from_pos):
   actions = []
-  new_hallway = state[4].copy()
-  new_hallway[index] = elem
-  cost = (steps_until_hallway + abs(from_pos - index)) * step_cost_by[elem]
-  _state = [state[0].copy(), state[1].copy(), state[2].copy(), state[3].copy(), new_hallway]
-  actions.append((cost, _state))
+  if (not index in [room_pos['A'], room_pos['B'], room_pos['C'], room_pos['D']]): #NOT SURE
+    new_hallway = state[4].copy()
+    new_hallway[index] = elem
+    cost = (steps_until_hallway + abs(from_pos - index)) * step_cost_by[elem]
+    _state = [state[0].copy(), state[1].copy(), state[2].copy(), state[3].copy(), new_hallway]
+    actions.append((cost, _state))
   actions = actions + case_to_enter_on_room(index, state, elem, steps_until_hallway, from_pos)
   return actions
 
 def case_to_enter_on_room(hallway_index, current_state, elem, steps_until_hallway, from_pos):
-  all_mines = all(map(lambda x: goal[x] == goal[elem], current_state[goal[elem]]))
-  if (hallway_index == room_pos[elem] and len(current_state[goal[elem]]) > 0 and all_mines):
+  all_like_me = all(map(lambda x: x == elem, current_state[goal[elem]]))
+  if (hallway_index == room_pos[elem] and all_like_me):
     steps_to_enter = 1 if len(current_state[goal[elem]]) == 1 else 2
     _hallway = current_state[4].copy()
     cost = (steps_until_hallway + (from_pos - hallway_index) + steps_to_enter) * step_cost_by[elem]
     __state = [current_state[0].copy(), current_state[1].copy(), current_state[2].copy(), current_state[3].copy(), _hallway]
-    __state[goal[elem]].append(elem)
+    __state[goal[elem]].insert(0, elem)
     return [(cost, __state)]
   else: return []
 
@@ -49,7 +50,7 @@ def actions_on_room(state, room_index, room):
   if len(state[room_index]) == 0: return []
   steps_until_hallway = 1 if len(state[room_index]) == 2 else 2
   copied_state = deepcopy(state)
-  head = copied_state[room_index].pop() # Modified copied
+  head = copied_state[room_index].pop(0) # Modified copied
 
   for i in reversed(range(room_pos[room])):
     if (state[4][i] != '.'): break # case when there is another Letter
--- challenges/Day 23: Amphipod/hanoi.py
+++ challenges/Day 23: Amphipod/hanoi.py
@@ -8,27 +8,24 @@ with open(input_path) as f:
   for row, line in enumerate(f):
     for col, c in enumerate(line[:-1]):
       if row in range(2, 4):
-        if col == room_pos['A']: A.append(c)
-        elif col == room_pos['B']: B.append(c)
-        elif col == room_pos['C']: C.append(c)
-        elif col == room_pos['D']: D.append(c)
+        if col == room_pos['A'] + 1: A.append(c)
+        elif col == room_pos['B'] + 1: B.append(c)
+        elif col == room_pos['C'] + 1: C.append(c)
+        elif col == room_pos['D'] + 1: D.append(c)
 
 def play_bfs(graph, initial_node, is_goal):
   queue = deque()
   visited = set()
   queue.append((initial_node, 0))
   visited.add(str(initial_node))
+  costs = []
   while(queue):
     node, cost = queue.popleft()
-    print('node, cost ', node, cost )
-    if is_goal(node):
-      print('solution: ', node, cost)
-      # continue
-      return node, cost
+    if is_goal(node): costs.append(cost); continue
     for state_cost, next in graph[node]:
       if str(next) not in visited:
         visited.add(str(next))
         queue.append((next, cost + state_cost))
-  return None
+  return costs
 
-print(play_bfs(Game(), initial_state(A, B, C, D), is_goal))
+print(min(play_bfs(Game(), initial_state(A, B, C, D), is_goal)))
