d5b0af8760fe52fe9a91887477cb13c7cb203766
Federico Lochbaum <federico.lochbaum@gmail.com>
2022-01-03 10:45:51 -0300

    some refaactor

--- challenges/Day 23: Amphipod/game.py
+++ challenges/Day 23: Amphipod/game.py
@@ -6,15 +6,14 @@ step_cost_by = { 'A': 1, 'B': 10, 'C': 100, 'D': 1000 }
 room_pos = { 'A': 2, 'B': 4, 'C': 6, 'D': 8 }
 goal = { 'A': 0, 'B': 1, 'C': 2, 'D': 3 }
 
-is_goal = lambda state: state == [['A', 'A'], ['B', 'B'], ['C', 'C'], ['D', 'D'], empty_hallway]
+is_goal = lambda state: state == [['A', 'A', 'A', 'A'], ['B', 'B', 'B', 'B'], ['C', 'C', 'C', 'C'], ['D', 'D', 'D', 'D'], empty_hallway]
 
 def hallway_actions_by_index(state, index, elem, steps_until_hallway, from_pos):
   actions = []
   if (not index in [room_pos['A'], room_pos['B'], room_pos['C'], room_pos['D']]):
-    new_hallway = state[4].copy()
-    new_hallway[index] = elem
+    _state = deepcopy(state)
+    _state[4][index] = elem
     cost = (steps_until_hallway + abs(from_pos - index)) * step_cost_by[elem]
-    _state = [state[0].copy(), state[1].copy(), state[2].copy(), state[3].copy(), new_hallway]
     actions.append((cost, _state))
   actions = actions + case_to_enter_on_room(state, index, elem, steps_until_hallway, from_pos)
   return actions
@@ -22,10 +21,9 @@ def hallway_actions_by_index(state, index, elem, steps_until_hallway, from_pos):
 def case_to_enter_on_room(current_state, hallway_index, elem, steps_until_hallway, from_pos):
   all_like_me = all(map(lambda x: x == elem, current_state[goal[elem]]))
   if (hallway_index == room_pos[elem] and all_like_me):
-    steps_to_enter = 1 if len(current_state[goal[elem]]) == 1 else 2
-    _hallway = current_state[4].copy()
+    steps_to_enter = abs(len(current_state[goal[elem]]) - 4)
     cost = (steps_until_hallway + abs(from_pos - hallway_index) + steps_to_enter) * step_cost_by[elem]
-    __state = [current_state[0].copy(), current_state[1].copy(), current_state[2].copy(), current_state[3].copy(), _hallway]
+    __state = deepcopy(current_state)
     __state[goal[elem]].insert(0, elem)
     return [(cost, __state)]
   else: return []
@@ -36,20 +34,17 @@ def hallway_actions(state):
     if elem != '.':
       copied_state = deepcopy(state)
       copied_state[4][pos] = '.' # Modified copied
-      all_like_me = all(map(lambda x: x == elem, copied_state[goal[elem]]))
-      if all_like_me:
-        steps_to_enter = 1 if len(copied_state[goal[elem]]) == 1 else 2
-        _hallway = copied_state[4].copy()
+      if all(map(lambda x: x == elem, copied_state[goal[elem]])):
+        steps_to_enter = abs(len(copied_state[goal[elem]]) - 4)
         cost = (abs(pos - room_pos[elem]) + steps_to_enter) * step_cost_by[elem]
-        __state = [copied_state[0].copy(), copied_state[1].copy(), copied_state[2].copy(), copied_state[3].copy(), _hallway]
-        __state[goal[elem]].insert(0, elem)
-        actions = actions + [(cost, __state)]
+        copied_state[goal[elem]].insert(0, elem)
+        actions = actions + [(cost, copied_state)]
   return actions
 
 def actions_on_room(state, room_index, room):
   actions = []
   if len(state[room_index]) == 0: return []
-  steps_until_hallway = 1 if len(state[room_index]) == 2 else 2
+  steps_until_hallway = abs(len(state[room_index]) - 4) + 1
   copied_state = deepcopy(state)
   head = copied_state[room_index].pop(0) # Modified copied
 
--- challenges/Day 23: Amphipod/hanoi.py
+++ challenges/Day 23: Amphipod/hanoi.py
@@ -1,4 +1,4 @@
-input_path = 'advent_of_code_2021/challenges/Day 23: Amphipod/input'
+input_path = 'advent_of_code_2021/challenges/Day 23: Amphipod/test2'
 from game import Game, initial_state, room_pos, is_goal
 from collections import deque
 
@@ -7,7 +7,7 @@ memoization = {}
 with open(input_path) as f:
   for row, line in enumerate(f):
     for col, c in enumerate(line[:-1]):
-      if row in range(2, 4):
+      if row in range(2, 6):
         if col == room_pos['A'] + 1: A.append(c)
         elif col == room_pos['B'] + 1: B.append(c)
         elif col == room_pos['C'] + 1: C.append(c)
@@ -33,4 +33,25 @@ def play_bfs(graph, initial_node, is_goal):
         queue.append((next, n_cost))
   return min_cost
 
-print(play_bfs(Game(), initial_state(A, B, C, D), is_goal))
+def play_dfs(graph, initial_node, is_goal):
+  stack = deque()
+  visited = set()
+  stack.append((initial_node, 0))
+  visited.add((str(initial_node), 0))
+  min_cost = float('inf')
+  while(stack):
+    node, cost = stack.pop()
+    if is_goal(node):
+      print('a comparar: ', cost, min_cost)
+      min_cost = min(min_cost, cost); continue
+    if cost >= min_cost: continue
+    for state_cost, next in graph[node]:
+      n_cost = cost + state_cost
+      if n_cost >= min_cost: continue
+      if (str(next), n_cost) not in visited:
+        visited.add((str(next), n_cost))
+        stack.append((next, n_cost))
+  return min_cost
+
+# print(play_bfs(Game(), initial_state(A, B, C, D), is_goal))
+print(play_dfs(Game(), initial_state(A, B, C, D), is_goal))
