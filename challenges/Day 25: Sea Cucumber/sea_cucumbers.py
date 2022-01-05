9d6f5fac85316dc9a08a25b6e844a207a0d904c5
Federico Lochbaum <federico.lochbaum@gmail.com>
2022-01-05 18:11:54 -0300

    advances

--- challenges/Day 25: Sea Cucumber/sea_cucumbers.py
+++ challenges/Day 25: Sea Cucumber/sea_cucumbers.py
@@ -10,49 +10,43 @@ with open(input_path) as f:
     for c in line[:-1]: map[i].append(c)
 
 def can_move_right(r, c, map, mutable_map):
-  return (len(mutable_map[r]) - 1 == c and map[r][0] == '.') or map[r][c + 1] == '.'
+  if len(mutable_map[r]) - 1 == c: return map[r][0] == '.'
+
+  return map[r][c + 1] == '.'
 
 def can_move_bottom(r, c, map, mutable_map):
-  if len(mutable_map) - 1 == r:
-    return mutable_map[0][c] == '.'
-  elif mutable_map[r + 1][c] == '.':
+  if len(mutable_map) - 1 == r: return map[0][c] == '.' or (map[0][c] == '>' and can_move_right(0, c, map, mutable_map))
+  elif map[r + 1][c] == '.':
     if c == 0: return map[r+1][len(mutable_map[r+1]) - 1] != '>'
     if map[r+1][c - 1] == '>': return False
     return True
-  elif mutable_map[r + 1][c] == '>':
+  elif map[r + 1][c] == '>':
     return can_move_right(r+1, c, map, mutable_map)
 
   return False
 
 def move_bottom(r, c, map, mutable_map):
   # case where mutable_map[r][c] = v
-  if len(mutable_map) - 1 == r: # Limit case
-    if mutable_map[0][c] == '.':
-      mutable_map[0][c] = 'v'
-      mutable_map[r][c] = '.'
-      return True
-
   if can_move_bottom(r, c, map, mutable_map):
-    mutable_map[r+1][c] = 'v'
+    if len(mutable_map) - 1 == r:
+      mutable_map[0][c] = 'v'
+    else:
+      mutable_map[r+1][c] = 'v'
     mutable_map[r][c] = '.'
     return True
 
   return False
 
-# Remember, the cucumbers move simultaneously
 def move_right(r, c, map, mutable_map):
   # case where mutable_map[r][c] = >
-  if len(mutable_map[r]) - 1 == c: # Limit case
-    if map[r][0] == '.':
+  if can_move_right(r, c, map, mutable_map):
+    if len(mutable_map[r]) - 1 == c:
       mutable_map[r][0] = '>'
-      mutable_map[r][c] = '.' if mutable_map[r][c] == map[r][c] else mutable_map[r][c]
-      return True
-    return False
-
-  if map[r][c + 1] == '.':
-    mutable_map[r][c+1] =  '>'
+    else:
+      mutable_map[r][c+1] = '>'
     mutable_map[r][c] = '.' if mutable_map[r][c] == map[r][c] else mutable_map[r][c]
     return True
+
   return False
 
 def step(map):
@@ -66,7 +60,7 @@ def step(map):
   return mutable_map, movements
 
 movements = None
-while(movements != 0 and steps < 2):
+while(movements != 0 and steps < 3):
   map, movements = step(map); steps += 1
   for i in map: print(''.join(i))
   print('\n')
